9c617b8eb57df8a3a20516a0446247ab65594321
zhaohua <1127195527@qq.com>
2016-07-19 11:21:06 +0800

    添加通过反射根据那么获取mipmap中id

--- app/src/main/java/com/vince/utils/BitmapUtils.java
+++ app/src/main/java/com/vince/utils/BitmapUtils.java
@@ -5,16 +5,21 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+
+import com.vince.example.utils.R;
+
 import java.io.BufferedOutputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.util.Arrays;
 
 /**
  * decription ：
+ *
  * @author ： vince
  */
 public class BitmapUtils {
@@ -24,7 +29,7 @@ public class BitmapUtils {
     //从Assert中获取bitmap
     private Bitmap getDrawableBitmap(String name) {
         Bitmap bm = null;
-        if(mAssetManager != null) {
+        if (mAssetManager != null) {
             InputStream assetFile = null;
 
             try {
@@ -42,7 +47,7 @@ public class BitmapUtils {
     //从Assert中获取Drawable
     private Drawable getDrawable(String name) {
         BitmapDrawable da = null;
-        if(mAssetManager != null) {
+        if (mAssetManager != null) {
             InputStream assetFile = null;
 
             try {
@@ -60,28 +65,28 @@ public class BitmapUtils {
 
 
     //保存bitmap
-    public boolean saveBitmap( String filePath, Bitmap bitmap, Bitmap.CompressFormat format, boolean needPrompt ){
+    public boolean saveBitmap(String filePath, Bitmap bitmap, Bitmap.CompressFormat format, boolean needPrompt) {
         long time = System.currentTimeMillis();
         boolean isSaveSuccess = false;
-        File file = new File( filePath );
-        if( null == bitmap ){
+        File file = new File(filePath);
+        if (null == bitmap) {
             file.delete();
             return true;
         }
 
         DataOutputStream cout = null;
 
-        try{
-            cout = new DataOutputStream( new BufferedOutputStream( new FileOutputStream( file ) ) );
-            isSaveSuccess = bitmap.compress( format, 100, cout );
+        try {
+            cout = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(file)));
+            isSaveSuccess = bitmap.compress(format, 100, cout);
             cout.flush();
-        } catch( IOException e ){
+        } catch (IOException e) {
             DebugPrint.printLogE(Arrays.toString(e.getStackTrace()));
         } finally {
-            if( cout != null ){
-                try{
+            if (cout != null) {
+                try {
                     cout.close();
-                } catch( IOException e ){
+                } catch (IOException e) {
                     DebugPrint.printLogE(Arrays.toString(e.getStackTrace()));
                 }
             }
@@ -90,5 +95,24 @@ public class BitmapUtils {
         time = System.currentTimeMillis() - time;
 
         return isSaveSuccess;
+    }
+
+    /**
+     * 根据文件名获取mipmap下图片文件的id
+     *
+     * @param imgName
+     */
+    public int getImageResourceId(String imgName) {
+
+        Class drawable = R.drawable.class;
+        Field field;
+        int res_ID = 0;
+        try {
+            field = drawable.getField("background");
+            res_ID = field.getInt(field.getName());
+        } catch (Exception e) {
+        }
+
+        return res_ID;
     }
 }
